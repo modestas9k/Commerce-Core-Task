cd56c9b116199cf46f7020b2fffce63d5fbe3b0e
Modestas Skirius <modestas.skirius@gmail.com>
2021-12-19 21:29:47 +0200

    change default font

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,7 +16,7 @@
 
         <style>
             body {
-                font-family: 'Nunito', sans-serif;
+                font-family: 'Roboto', sans-serif;
             }
         </style>
     </head>
